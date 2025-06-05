6fc90c2cb4e923de2a8ea650c309d8616312d684
bienvenudev <cbienvenu007@gmail.com>
2025-06-05 11:40:14 +0200

    feat: enhance dashboard with new interfaces and class

--- dist/index.js
+++ dist/index.js
@@ -1,125 +1,139 @@
-// Interfaces
-// 1. Based on what we have learnt in this lesson, and what we discussed in the previous one,
-// please get rid of the uncessary code in this project and replace it with the Review interface
-// 2. Move the interface to its own file for the next lesson
-import { showReviewTotal, populateUser, showDetails, getTopTwoReviews } from './utils.js';
-const propertyContainer = document.querySelector('.properties');
-const reviewContainer = document.querySelector('.reviews');
-const container = document.querySelector('.container');
-const button = document.querySelector('button');
-const footer = document.querySelector('.footer');
+// Wrapping up our Dashboard
+// 1. Create All the other interfaces you think are needed for this board
+// 2. Using the Class, visually show the main Image above the review button.
+import { showReviewTotal, populateUser, showDetails, getTopTwoReviews, } from "./utils.js";
+import { Permissions, LoyaltyUser } from "./enums.js";
+const propertyContainer = document.querySelector(".properties");
+const reviewContainer = document.querySelector(".reviews");
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+const footer = document.querySelector(".footer");
 let isLoggedIn;
-var Permissions;
-(function (Permissions) {
-    Permissions["ADMIN"] = "ADMIN";
-    Permissions["READ_ONLY"] = "READ_ONLY";
-})(Permissions || (Permissions = {}));
-var LoyaltyUser;
-(function (LoyaltyUser) {
-    LoyaltyUser["GOLD_USER"] = "GOLD_USER";
-    LoyaltyUser["SILVER_USER"] = "SILVER_USER";
-    LoyaltyUser["BRONZE_USER"] = "BRONZE_USER";
-})(LoyaltyUser || (LoyaltyUser = {}));
 // Reviews
 const reviews = [
     {
-        name: 'Sheia',
+        name: "Sheila",
         stars: 5,
         loyaltyUser: LoyaltyUser.GOLD_USER,
-        date: '01-04-2021'
+        date: "01-04-2021",
     },
     {
-        name: 'Andrzej',
+        name: "Andrzej",
         stars: 3,
         loyaltyUser: LoyaltyUser.BRONZE_USER,
-        date: '28-03-2021'
+        date: "28-03-2021",
     },
     {
-        name: 'Omar',
+        name: "Omar",
         stars: 4,
         loyaltyUser: LoyaltyUser.SILVER_USER,
-        date: '27-03-2021',
+        date: "27-03-2021",
     },
 ];
 const you = {
-    firstName: 'Bobby',
-    lastName: 'Brown',
+    firstName: "Bobby",
+    lastName: "Brown",
     permissions: Permissions.ADMIN,
     isReturning: true,
     age: 35,
-    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+    stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
 // Array of Properties
 const properties = [
     {
-        image: 'images/colombia-property.jpg',
-        title: 'Colombian Shack',
+        image: "images/colombia-property.jpg",
+        title: "Colombian Shack",
         price: 45,
         location: {
-            firstLine: 'shack 37',
-            city: 'Bogota',
+            firstLine: "shack 37",
+            city: "Bogota",
             code: 45632,
-            country: 'Colombia'
+            country: "Colombia",
         },
-        contact: [+112343823978921, 'marywinkle@gmail.com'],
-        isAvailable: true
+        contact: [+112343823978921, "marywinkle@gmail.com"],
+        isAvailable: true,
     },
     {
-        image: 'images/poland-property.jpg',
-        title: 'Polish Cottage',
-        price: 34,
+        image: "images/poland-property.jpg",
+        title: "Polish Cottage",
+        price: 30,
         location: {
-            firstLine: 'no 23',
-            city: 'Gdansk',
+            firstLine: "no 23",
+            city: "Gdansk",
             code: 343903,
-            country: 'Poland'
+            country: "Poland",
         },
-        contact: [+1298239028490830, 'garydavis@hotmail.com'],
-        isAvailable: false
+        contact: [+1298239028490830, "garydavis@hotmail.com"],
+        isAvailable: false,
     },
     {
-        image: 'images/london-property.jpg',
-        title: 'London Flat',
-        price: 23,
+        image: "images/london-property.jpg",
+        title: "London Flat",
+        price: 25,
         location: {
-            firstLine: 'flat 15',
-            city: 'London',
+            firstLine: "flat 15",
+            city: "London",
             code: 35433,
-            country: 'United Kingdom',
+            country: "United Kingdom",
         },
-        contact: [+34829374892553, 'andyluger@aol.com'],
-        isAvailable: true
-    }
+        contact: [+34829374892553, "andyluger@aol.com"],
+        isAvailable: true,
+    },
 ];
 // Functions
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 populateUser(you.isReturning, you.firstName);
 // Add the properties
 for (let i = 0; i < properties.length; i++) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const card = document.createElement("div");
+    card.classList.add("card");
     card.innerHTML = properties[i].title;
-    const image = document.createElement('img');
-    image.setAttribute('src', properties[i].image);
+    const image = document.createElement("img");
+    image.setAttribute("src", properties[i].image);
     card.appendChild(image);
     showDetails(you.permissions, card, properties[i].price);
     propertyContainer.appendChild(card);
 }
-//Broken code
 let count = 0;
 function addReviews(array) {
     if (!count) {
         count++;
         const topTwo = getTopTwoReviews(array);
         for (let i = 0; i < topTwo.length; i++) {
-            const card = document.createElement('div');
-            card.classList.add('review-card');
-            card.innerHTML = topTwo[i].stars + ' stars from ' + topTwo[i].name;
+            const card = document.createElement("div");
+            card.classList.add("review-card");
+            card.innerHTML = topTwo[i].stars + " stars from " + topTwo[i].name;
             reviewContainer.appendChild(card);
         }
         container.removeChild(button);
     }
 }
-button.addEventListener('click', () => addReviews(reviews));
-let currentLocation = ['London', '11.03', 17];
-footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°';
+button.addEventListener("click", () => addReviews(reviews));
+let currentLocation = ["London", "11.03", 17];
+footer.innerHTML =
+    currentLocation[0] +
+        " " +
+        currentLocation[1] +
+        " " +
+        currentLocation[2] +
+        "°";
+// Classes
+class MainProperty {
+    constructor(src, title, reviews) {
+        this.src = src;
+        this.title = title;
+        this.reviews = reviews;
+    }
+}
+let yourMainProperty = new MainProperty("images/colombia-property.jpg", "Italian House", [
+    {
+        name: "Olive",
+        stars: 5,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
+        date: "05-06-2025",
+    },
+]);
+const mainImageContainer = document.querySelector(".main-image");
+const image = document.createElement("img");
+image.setAttribute("src", yourMainProperty.src);
+mainImageContainer === null || mainImageContainer === void 0 ? void 0 : mainImageContainer.appendChild(image);
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,14 +1,23 @@
 import { LoyaltyUser } from "./enums.js";
+import { Price, Country } from "./types.js";
 
-export interface Review  {
- name: string; 
-    stars: number; 
-    loyaltyUser: LoyaltyUser; 
-    date: string; 
+export interface Review {
+  name: string;
+  stars: number;
+  loyaltyUser: LoyaltyUser;
+  date: string;
 }
 
-export interface Property {
-    src: string;
-    title: string;
-    reviews: Review[];
+export interface Properties {
+  image: string;
+  title: string;
+  price: Price;
+  location: {
+    firstLine: string;
+    city: string;
+    code: number;
+    country: Country;
+  };
+  contact: [number, string];
+  isAvailable: boolean;
 }
