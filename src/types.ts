2646bc1062a58629246eb9df949a44162e1a21bd
bienvenudev <cbienvenu007@gmail.com>
2025-06-05 12:08:28 +0200

    feat: add type definitions for property pricing and countries

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
 // type alias
-export type Price = 45 | 30 | 25;
-export type Country = "Colombia" | "Poland" | "United Kingdom";
+export type Price = 45 | 30 | 35 | 25;
+export type Country = "Colombia" | "Poland" | "United Kingdom" | "Malaysia";
